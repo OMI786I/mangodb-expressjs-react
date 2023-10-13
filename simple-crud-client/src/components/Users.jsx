c81fc16561c343001ebc74cb6910311fc58d52e6
OMI786I <sohrawardy1998@gmail.com>
2023-10-13 20:51:31 +0600

    updated delete functionality

--- simple-crud-client/src/components/Users.jsx
+++ simple-crud-client/src/components/Users.jsx
@@ -2,9 +2,8 @@ import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
-  const [visible, setVisible] = useState(true);
-
-  const users = useLoaderData();
+  const loadedUsers = useLoaderData();
+  const [users, setUsers] = useState(loadedUsers);
   const handleDelete = (_id) => {
     console.log("delete", _id);
     fetch(`http://localhost:5000/users/${_id}`, {
@@ -15,9 +14,10 @@ const Users = () => {
         console.log(data);
         if (data.deletedCount > 0) {
           alert("deleted successfully");
+          const remaining = users.filter((user) => user._id !== _id);
+          setUsers(remaining);
         }
       });
-    setVisible((prev) => !prev);
   };
   return (
     <div>
