c4ad4082d5c5078bcdedbbeb032d6d030d7ca95b
OMI786I <sohrawardy1998@gmail.com>
2023-10-13 11:45:20 +0600

    made a simple form in react

--- simple-crud-client/src/App.jsx
+++ simple-crud-client/src/App.jsx
@@ -1,11 +1,31 @@
-import { useState } from "react";
-
 import "./App.css";
 
 function App() {
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
+  };
+
   return (
     <div>
       <h1>Simple Crud</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" placeholder="enter your name" />
+        <br />
+        <input type="email" name="email" placeholder="enter your email" />
+        <br />
+        <input
+          type="password"
+          name="password"
+          placeholder="enter your password"
+        />
+        <br />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
